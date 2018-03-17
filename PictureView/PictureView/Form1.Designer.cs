7eeffed2b3284ec5361043907b7911dc478e56de
imweiwei <2218904390@qq.com>
2018-03-17 23:18:54 +0800

    pictureView code

---
+++ PictureView/PictureView/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace PictureView
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnZoonOut = new System.Windows.Forms.Button();
+            this.btnZoomIn = new System.Windows.Forms.Button();
+            this.btncounterclockwiseRotate = new System.Windows.Forms.Button();
+            this.btnClockwiseRotate = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnPre = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(69, 488);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "打开图片";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(150, 488);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "清除图片";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(3, 492);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(66, 16);
+            this.checkBox1.TabIndex = 13;
+            this.checkBox1.Text = "Stretch";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // closeButton
+            // 
+            this.closeButton.AutoSize = true;
+            this.closeButton.Location = new System.Drawing.Point(577, 489);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(49, 22);
+            this.closeButton.TabIndex = 14;
+            this.closeButton.Text = "关闭";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "JPEG Files (*.jpg)|*.jpg|PGN Files(*.png)|*.pgn|BMP Files(*bmp)|*.bmp|All files(*" +
+    ".*)|*.*";
+            this.openFileDialog1.Title = "Select a picture file";
+            // 
+            // btnZoonOut
+            // 
+            this.btnZoonOut.Image = global::PictureView.Properties.Resources.btnZoomOut;
+            this.btnZoonOut.Location = new System.Drawing.Point(272, 487);
+            this.btnZoonOut.Name = "btnZoonOut";
+            this.btnZoonOut.Size = new System.Drawing.Size(25, 23);
+            this.btnZoonOut.TabIndex = 16;
+            this.btnZoonOut.UseVisualStyleBackColor = true;
+            this.btnZoonOut.Click += new System.EventHandler(this.btnZoonOut_Click);
+            // 
+            // btnZoomIn
+            // 
+            this.btnZoomIn.Image = global::PictureView.Properties.Resources.btnZoomIn;
+            this.btnZoomIn.Location = new System.Drawing.Point(241, 487);
+            this.btnZoomIn.Name = "btnZoomIn";
+            this.btnZoomIn.Size = new System.Drawing.Size(25, 23);
+            this.btnZoomIn.TabIndex = 15;
+            this.btnZoomIn.UseVisualStyleBackColor = true;
+            this.btnZoomIn.Click += new System.EventHandler(this.btnZoomIn_Click);
+            // 
+            // btncounterclockwiseRotate
+            // 
+            this.btncounterclockwiseRotate.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btncounterclockwiseRotate.BackgroundImage")));
+            this.btncounterclockwiseRotate.Location = new System.Drawing.Point(335, 488);
+            this.btncounterclockwiseRotate.Name = "btncounterclockwiseRotate";
+            this.btncounterclockwiseRotate.Size = new System.Drawing.Size(25, 23);
+            this.btncounterclockwiseRotate.TabIndex = 12;
+            this.btncounterclockwiseRotate.UseVisualStyleBackColor = true;
+            this.btncounterclockwiseRotate.Click += new System.EventHandler(this.btncounterclockwiseRotate_Click);
+            // 
+            // btnClockwiseRotate
+            // 
+            this.btnClockwiseRotate.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnClockwiseRotate.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnClockwiseRotate.BackgroundImage")));
+            this.btnClockwiseRotate.Location = new System.Drawing.Point(303, 488);
+            this.btnClockwiseRotate.Name = "btnClockwiseRotate";
+            this.btnClockwiseRotate.Size = new System.Drawing.Size(26, 23);
+            this.btnClockwiseRotate.TabIndex = 11;
+            this.btnClockwiseRotate.UseVisualStyleBackColor = true;
+            this.btnClockwiseRotate.Click += new System.EventHandler(this.btnClockwiseRotate_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnNext.Image = ((System.Drawing.Image)(resources.GetObject("btnNext.Image")));
+            this.btnNext.Location = new System.Drawing.Point(434, 488);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(47, 23);
+            this.btnNext.TabIndex = 10;
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnPre
+            // 
+            this.btnPre.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnPre.Image = ((System.Drawing.Image)(resources.GetObject("btnPre.Image")));
+            this.btnPre.Location = new System.Drawing.Point(378, 488);
+            this.btnPre.Name = "btnPre";
+            this.btnPre.Size = new System.Drawing.Size(50, 23);
+            this.btnPre.TabIndex = 9;
+            this.btnPre.UseVisualStyleBackColor = true;
+            this.btnPre.Click += new System.EventHandler(this.btnPre_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.pictureBox1.Location = new System.Drawing.Point(1, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(627, 487);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(496, 489);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "播放幻灯片";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(628, 517);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnZoonOut);
+            this.Controls.Add(this.btnZoomIn);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.btncounterclockwiseRotate);
+            this.Controls.Add(this.btnClockwiseRotate);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPre);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnOpen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnPre;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnClockwiseRotate;
+        private System.Windows.Forms.Button btncounterclockwiseRotate;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnZoomIn;
+        private System.Windows.Forms.Button btnZoonOut;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ PictureView/PictureView/Form1.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PictureView
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private int begin_x;      
+        private int begin_y;
+
+
+        private Image image_ori;
+        private float zoom;           
+
+        private Point m_StarPoint = Point.Empty;      
+        private Point m_ViewPoint = Point.Empty;
+
+        int w;                      
+        int h;
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDlg = new OpenFileDialog();
+            openFileDlg.Filter = "*.jpg|*.jpg;*.jpeg";
+            if (openFileDlg.ShowDialog() == DialogResult.OK)
+            {
+                image_ori = Image.FromFile(openFileDlg.FileName);
+                begin_x = 0;
+                begin_y = 0;
+                zoom = 100;
+                zoom_image(false);
+            }
+            openFileDlg.Dispose();
+        }
+
+        private void zoom_image(bool chec)
+        {
+            w = Convert.ToInt32(image_ori.Width * zoom / 100);
+            h = Convert.ToInt32(image_ori.Height * zoom / 100);
+            if (w < 1 || h < 1) return;
+            if (chec)
+            {
+                if (begin_x + pictureBox1.Width > w) begin_x = w - pictureBox1.Width;
+                if (begin_y + pictureBox1.Height > h) begin_y = h - pictureBox1.Height;
+                if (begin_x < 0) begin_x = 0;
+                if (begin_y < 0) begin_y = 0;
+            }
+            Bitmap resizedBmp = new Bitmap(w, h);
+            Graphics g = Graphics.FromImage(resizedBmp);
+            g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.None;
+            g.DrawImage(image_ori, new Rectangle(0, 0, w, h), new Rectangle(0, 0, image_ori.Width, image_ori.Height), GraphicsUnit.Pixel);
+
+            int ww, hh;
+            ww = w;
+            hh = h;
+            if (pictureBox1.Width < ww) ww = pictureBox1.Width;
+            if (pictureBox1.Height < hh) hh = pictureBox1.Height;
+            try
+            {
+                pictureBox1.Image = resizedBmp.Clone(new RectangleF((float)begin_x, (float)begin_y, ww, hh), PixelFormat.Format24bppRgb);  
+            }
+            catch
+            {
+
+            }
+            g.Dispose();
+        }
+
+        
+
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            else
+                pictureBox1.SizeMode = PictureBoxSizeMode.Normal;
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                image_ori = Image.FromFile(openFileDialog1.FileName);
+                begin_x = 0;
+                begin_y = 0;
+                zoom = 100;
+                zoom_image(false);
+            }
+
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnZoomIn_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image == null)
+            {
+                return;
+            }
+            float min = pictureBox1.Width / image_ori.Width;
+            float min_y = pictureBox1.Height / image_ori.Height;
+            if (min < min_y) min = min_y;
+            else min = min_y;
+
+            zoom += 10;
+            if (zoom > 200) zoom = 200;
+            zoom_image(true);
+        }
+
+        private void btnZoonOut_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image == null)
+            {
+                return;
+            }
+            float min = pictureBox1.Width / image_ori.Width;
+            float min_y = pictureBox1.Height / image_ori.Height;
+            if (min < min_y) min = min_y;
+            else min = min_y;
+
+            zoom -= 10;
+            if (zoom < min) zoom = min;
+            zoom_image(true);
+        }
+
+        private void btnClockwiseRotate_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image == null)
+            {
+                return;
+            }
+            Image image = pictureBox1.Image;
+            image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+            image_ori = image;
+            begin_x = 0;
+            begin_y = 0;
+            zoom = 100;
+            zoom_image(false);
+        }
+
+        private void btnPre_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image == null)
+            {
+                return;
+            }
+            string[] path = Directory.GetFiles(openFileDialog1.FileName.Substring(0, openFileDialog1.FileName.LastIndexOf("\\") + 1));
+            for (int i = 0; i < path.Length; i++)
+            {
+                if (path[i] == openFileDialog1.FileName)
+                {
+                    i = i - 1;
+                    if (i < 0)
+                    {
+                        i = path.Length-1;
+                    }
+                    openFileDialog1.FileName = path[i];
+                    image_ori = Image.FromFile(path[i]);
+                    begin_x = 0;
+                    begin_y = 0;
+                    zoom = 100;
+                    zoom_image(false);
+                    return;
+                }
+
+            }
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image == null)
+            {
+                return;
+            }
+            string[] path = Directory.GetFiles(openFileDialog1.FileName.Substring(0, openFileDialog1.FileName.LastIndexOf("\\")+1));
+            for(int i = 0; i < path.Length; i++)
+            {
+                if (path[i] ==openFileDialog1.FileName)
+                {
+                    i=i+1;
+                    if (i > path.Length - 1)
+                    {
+                        i = 0;
+                    }
+                    openFileDialog1.FileName = path[i];
+                    image_ori = Image.FromFile(path[i]);
+                    begin_x = 0;
+                    begin_y = 0;
+                    zoom = 100;
+                    zoom_image(false);
+                    return;
+                }
+                
+            }
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image == null)
+            {
+                return;
+            }
+            if (timer1.Enabled == true)
+            {
+                timer1.Enabled = false;
+                button1.Text = "播放幻灯片";
+            }
+            else
+            {
+                timer1.Enabled = true;
+                button1.Text = "暂停播放";
+            }
+        }
+
+        private void btncounterclockwiseRotate_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image == null)
+            {
+                return;
+            }
+            Image image = pictureBox1.Image;
+            image.RotateFlip(RotateFlipType.Rotate270FlipNone);
+            image_ori = image;
+            begin_x = 0;
+            begin_y = 0;
+            zoom = 100;
+            zoom_image(false);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image == null)
+            {
+                return;
+            }
+            string[] path = Directory.GetFiles(openFileDialog1.FileName.Substring(0, openFileDialog1.FileName.LastIndexOf("\\") + 1));
+            for (int i = 0; i < path.Length; i++)
+            {
+                if (path[i] == openFileDialog1.FileName)
+                {
+                    i = i + 1;
+                    if (i > path.Length - 1)
+                    {
+                        i = 0;
+                    }
+                    openFileDialog1.FileName = path[i];
+                    image_ori = Image.FromFile(path[i]);
+                    begin_x = 0;
+                    begin_y = 0;
+                    zoom = 100;
+                    zoom_image(false);
+                }
+            }
+        }
+    }
+}
